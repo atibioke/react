96af974b4d2503dd36a1b0a8cf3c35778a5f7117
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-05-18 07:08:16 +0100

    learning react

--- src/index.js
+++ src/index.js
@@ -1,17 +1,51 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import reactDOM from "react-dom";
+import './index.css'
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+function BookList() {
+  return (
+    <section className="booklist">
+      <Book />
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    </section>
+  );
+}
+
+const Book = () => {
+  return (
+    <article className="book">
+     
+      <Image></Image>
+      <Title/>
+      <Author/>
+    </article>
+  );
+};
+
+const Image = () => {
+  return (
+    <img
+      src="https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1"
+      alt=""
+    />
+  );
+};
+
+const Title = () =>  <h1>Book Image</h1>
+const Author = () =>  <h4>John Mason</h4>
+
+reactDOM.render(<BookList />, document.getElementById("root"));
+
+
+
+
+
+
+
+
+
+
+
+
+
+
