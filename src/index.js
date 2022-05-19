1e241c36dcdb6c7e546aae80c8330d650753b19b
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-05-19 15:00:39 +0100

    studying reactjs

--- src/index.js
+++ src/index.js
@@ -3,19 +3,21 @@ import reactDOM from "react-dom";
 import './index.css'
 
 
-const firstBook = {
+const books = [
+
+{
   title: "Book Image",
   author: "John Mason",
   img : "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1"
-}
+},
 
-const secondBook ={
+{
   title: "Mind Book",
   author: "Mark Mason",
   img: "https://media.istockphoto.com/photos/the-mind-can-never-get-enough-books-picture-id1182489137?s=612x612"
 }
 
-
+]
 
 function BookList() {
   return (
