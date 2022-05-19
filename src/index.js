63aa2cb9cc7f97fc578a92ba4fe902a0636c07a3
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-05-19 09:33:19 +0100

    studying reactjs

--- src/index.js
+++ src/index.js
@@ -2,36 +2,48 @@ import React from "react";
 import reactDOM from "react-dom";
 import './index.css'
 
+
+const firstBook = {
+  title: "Book Image",
+  author: "John Mason",
+  img : "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1"
+}
+
+const secondBook ={
+  title: "Mind Book",
+  author: "Mark Mason",
+  img: "https://media.istockphoto.com/photos/the-mind-can-never-get-enough-books-picture-id1182489137?s=612x612"
+}
+
+
+
 function BookList() {
   return (
     <section className="booklist">
-      <Book />
-
+      <Book title={firstBook.title} author={firstBook.author} img={firstBook.img}/>
+      <Book title={secondBook.title} author={secondBook.author} img={secondBook.img}/>
     </section>
   );
 }
 
-const Book = () => {
-  const title =  "Book Image"
-const author = "John Mason"
+const Book = (props) => {
+
+  const { img, title, author } = props; 
+ 
   return (
     <article className="book">
      
-      <Image></Image>
+     <img
+      src= {img}
+      alt=""
+    />
       <h1>{title}</h1>
       <h4>{author}</h4>
     </article>
   );
 };
 
-const Image = () => {
-  return (
-    <img
-      src="https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1"
-      alt=""
-    />
-  );
-};
+
 
 
 
