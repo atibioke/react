9dbdcf7ce87eba0f6d994fe141b24f4e9069143f
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-05-20 19:14:17 +0100

    studying reactjs

--- src/index.js
+++ src/index.js
@@ -36,12 +36,18 @@ function BookList() {
 
 const Book = (props) => {
   const { img, title, author } = props;
+  const clickHandler = (e) => {
+    alert('I clicked here')
+  }
 
   return (
     <article className="book">
       <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
+      <button onClick={clickHandler}>
+        Click here
+      </button>
     </article>
   );
 };
