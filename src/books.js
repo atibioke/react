574ab24d8771c922023807c9df32535c2b4a0c4a
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-05-20 20:16:01 +0100

    studying reactjs

---
+++ src/books.js
@@ -0,0 +1,23 @@
+
+
+export const books = [
+    {
+      id:1,
+      title: "Book Image",
+      author: "John Mason",
+      img: "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1",
+    },
+  
+    {
+      id:2,
+      title: "Mind Book",
+      author: "Mark Mason",
+      img: "https://media.istockphoto.com/photos/the-mind-can-never-get-enough-books-picture-id1182489137?s=612x612",
+    },
+    {
+      id:3,
+      title: "Old Book",
+      author: "Philip Mason",
+      img: "https://cdn.pixabay.com/photo/2015/11/19/21/10/glasses-1052010_960_720.jpg",
+    },
+  ];
--- src/index.js
+++ src/index.js
@@ -1,28 +1,8 @@
 import React from "react";
 import reactDOM from "react-dom";
 import "./index.css";
+import {books} from "./books"
 
-const books = [
-  {
-    id:1,
-    title: "Book Image",
-    author: "John Mason",
-    img: "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1",
-  },
-
-  {
-    id:2,
-    title: "Mind Book",
-    author: "Mark Mason",
-    img: "https://media.istockphoto.com/photos/the-mind-can-never-get-enough-books-picture-id1182489137?s=612x612",
-  },
-  {
-    id:3,
-    title: "Old Book",
-    author: "Philip Mason",
-    img: "https://cdn.pixabay.com/photo/2015/11/19/21/10/glasses-1052010_960_720.jpg",
-  },
-];
 
 function BookList() {
   return (
