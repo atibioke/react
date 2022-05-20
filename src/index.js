4262c455722e5e27507a195d7d1fd02e6d1bc368
Rasaq Ganiyu <eudoxusganium@gmail.com>
2022-05-20 19:01:04 +0100

    studying reactjs

--- src/index.js
+++ src/index.js
@@ -1,65 +1,49 @@
 import React from "react";
 import reactDOM from "react-dom";
-import './index.css'
-
+import "./index.css";
 
 const books = [
+  {
+    id:1,
+    title: "Book Image",
+    author: "John Mason",
+    img: "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1",
+  },
 
-{
-  title: "Book Image",
-  author: "John Mason",
-  img : "https://images.theconversation.com/files/45159/original/rptgtpxd-1396254731.jpg?ixlib=rb-1.1.0&q=45&auto=format&w=754&h=502&fit=crop&dpr=1"
-},
-
-{
-  title: "Mind Book",
-  author: "Mark Mason",
-  img: "https://media.istockphoto.com/photos/the-mind-can-never-get-enough-books-picture-id1182489137?s=612x612"
-}
-
-]
+  {
+    id:2,
+    title: "Mind Book",
+    author: "Mark Mason",
+    img: "https://media.istockphoto.com/photos/the-mind-can-never-get-enough-books-picture-id1182489137?s=612x612",
+  },
+  {
+    id:3,
+    title: "Old Book",
+    author: "Philip Mason",
+    img: "https://cdn.pixabay.com/photo/2015/11/19/21/10/glasses-1052010_960_720.jpg",
+  },
+];
 
 function BookList() {
   return (
     <section className="booklist">
-      
+      {books.map((book) => {
+        return <Book key={book.id} book={book} />;
+      })}
     </section>
   );
 }
 
 const Book = (props) => {
+  const { img, title, author } = props.book;
 
-  const { img, title, author, children } = props; 
- 
   return (
     <article className="book">
-     
-     <img
-      src= {img}
-      alt=""
-    />
+      <img src={img} alt="" />
       <h1>{title}</h1>
       <h4>{author}</h4>
-      {children}
     </article>
   );
 };
 
-
-
-
 reactDOM.render(<BookList />, document.getElementById("root"));
-
-
-
-
-
-
-
-
-
-
-
-
-
-
